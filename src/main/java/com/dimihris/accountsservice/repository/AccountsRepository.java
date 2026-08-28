2d3d6f0dc4e044557c0b2416428fbd8d6ee9dd9e
dimihris <hristo_dimitrov@flutterint.com>
2026-08-28 17:18:49 +0300

    fix include @Repository annotation on AccountsRepository

--- src/main/java/com/dimihris/accountsservice/repository/AccountsRepository.java
+++ src/main/java/com/dimihris/accountsservice/repository/AccountsRepository.java
@@ -2,6 +2,8 @@ package com.dimihris.accountsservice.repository;
 
 import com.dimihris.accountsservice.entity.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
 }
