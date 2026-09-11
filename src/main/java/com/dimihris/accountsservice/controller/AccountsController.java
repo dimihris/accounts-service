260ad79878d9c42f08d1ca012247c937995efa07
dimihris <hristo_dimitrov@flutterint.com>
2026-09-11 11:51:14 +0300

    chore: reading configurations using environment interface

--- src/main/java/com/dimihris/accountsservice/controller/AccountsController.java
+++ src/main/java/com/dimihris/accountsservice/controller/AccountsController.java
@@ -17,6 +17,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -41,6 +42,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountsController {
 
     private final AccountsService accountsService;
+
+    private final Environment environment;
 
     @Value("${build.version}")
     private String buildVersion;
@@ -271,5 +274,29 @@ public class AccountsController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(buildVersion);
+    }
+
+    @Operation(
+            summary = "Get Java Version",
+            description = "Get Java version details that is installed into accounts-service"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
     }
 }
